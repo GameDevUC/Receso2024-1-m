2f9f6296ae34f264dd92cea45cc57147bea460f1
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-04-30 22:48:08 -0400

    Feat: jumps and speeds
    Now Mario can jump different heights, depending on when the player stops pressing space and also there is a speed increase after the player has run on a single direction for long enough

--- characters/Mario/player.gd
+++ characters/Mario/player.gd
@@ -13,6 +13,8 @@ const JUMP_VELOCITY = -500.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 var direction = 0
+var speed_level = 0
+var time_running = 0
 
 var jumping = false
 
@@ -36,6 +38,10 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("jump") and is_on_floor():
 			animated_sprite.animation = jump
 			velocity.y = JUMP_VELOCITY
+			jumping = true
+		if not Input.is_action_pressed("jump") and not is_on_floor() and jumping == true:
+			velocity.y = 0
+			jumping = false
 
 		# Get the input direction and handle the movement/deceleration.
 		direction = Input.get_axis("move_left", "move_right")
@@ -48,11 +54,17 @@ func _physics_process(delta):
 				animated_sprite.animation = walk
 			
 		if direction:
+			if speed_level == 0: speed_level = 1
+			if is_on_floor(): time_running += delta
+			if time_running > 1 and speed_level < 3:
+				time_running = 0
+				speed_level += 1
 			if direction < 0: animated_sprite.flip_h = true
 			else: animated_sprite.flip_h = false
-			velocity.x = direction * SPEED
+			velocity.x = direction * SPEED * speed_level
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
+			speed_level = 0
 			
 	else:
 		animated_sprite.animation = crouch
