c2e19528913deae08f326ee765af5c9cfff72503
MystEdu <esep@uc.cl>
2024-05-02 10:22:48 -0400

    Random slide direction on dead (bugged)

--- characters/Koopa/Koopa.gd
+++ characters/Koopa/Koopa.gd
@@ -16,7 +16,11 @@ var direction = 1
 var dead = false
 var atacking = false
 
+@onready var life_time = $LifeTime
+
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var collision_shape = $CollisionShape2D
+@onready var dead_collision_shape = $DeadArea/CollisionShape2D
 
 @onready var ray_cast_direction = $RayCastDirection
 @onready var ray_cast_up = $RayCastUp
@@ -27,6 +31,7 @@ var atacking = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
+	dead_collision_shape.disabled = true
 	if direction > 0:
 		animated_sprite.flip_h = true
 	animated_sprite.animation = walk
@@ -42,8 +47,10 @@ func _process(delta):
 		direction = 0
 		animated_sprite.animation = dead_animation
 		dead = true
-		$CollisionShape2D.disabled = true # DELETE THIS when throw added
-		$LifeTime.start()
+		collision_shape.disabled = true
+		$ActiveDeadArea.start()
+		life_time.start()
+
 	
 func _physics_process(delta):
 	if not dead:
@@ -76,6 +83,15 @@ func _physics_process(delta):
 			velocity.x = direction * SPEED
 
 		move_and_slide()
+		
+	else:
+		if ray_cast_direction.is_colliding():
+			if ray_cast_direction.get_collider().name == "Player":
+				hit_player_signal.emit()
+			direction *= -1
+			ray_cast_direction.target_position.x *= -1
+		velocity.x = direction * SLIDE_SPEED
+		move_and_slide()
 
 
 func _on_life_time_timeout():
@@ -89,3 +105,18 @@ func _on_animated_sprite_2d_frame_changed():
 
 func _on_atack_time_timeout():
 	atacking = false
+
+
+func _on_dead_area_body_entered(body):
+	if body.name == "Player" and not dead_collision_shape.disabled:
+		collision_shape.disabled = false
+		dead_collision_shape.disabled = true
+		direction = randi_range(1, 2)
+		if direction == 2:
+			direction = -1
+		life_time.start()
+		
+
+
+func _on_active_dead_area_timeout():
+	dead_collision_shape.disabled = false
